040fa99050f6e948addefa07741c99a823c5331a
Tonyha7 <041202yuan@gmail.com>
2025-01-12 23:00:04 +0800

    auto card

---
+++ uCertify Auto Card-1.1.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         uCertify Auto Card
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Enable and auto-click the "Correct" button on uCertify flash card pages
+// @match        *.ucertify.com/app/?*get_flash_card*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function enableAndClickButton() {
+        const correctButton = document.getElementById('correct');
+        if (correctButton && correctButton.disabled) {
+            correctButton.disabled = false;
+            correctButton.click();
+        }
+    }
+
+    const interval = setInterval(() => {
+        enableAndClickButton();
+    }, 500);
+
+    setTimeout(() => {
+        clearInterval(interval);
+    }, 100000); // Stop after 100s
+})();
